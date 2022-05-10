ca43500c4ad215c1c28aa1eb800784b8c79b197e
prakharritik <60786008+prakharritik@users.noreply.github.com>
2022-05-10 23:30:52 +0530

    Add profile schema.

---
+++ server/models/Profile.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const ProfileSchema = new mongoose.Schema({
+  height: {
+    type: Number,
+    required: true,
+  },
+  weight: {
+    type: Number,
+    required: true,
+  },
+  age: {
+    type: Number,
+    required: true,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "users",
+  },
+});
+
+module.exports = mongoose.model("profile", ProfileSchema);
