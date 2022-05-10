0f0c5415405b18a850da5b0d1a0dde88f43f5810
prakharritik <60786008+prakharritik@users.noreply.github.com>
2022-05-10 20:00:11 +0530

    Update User model.

--- server/models/User.js
+++ server/models/User.js
@@ -14,6 +14,10 @@ const UserSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  emailVerified: {
+    type: Boolean,
+    default: false,
+  },
   date: {
     type: Date,
     default: Date.now,
