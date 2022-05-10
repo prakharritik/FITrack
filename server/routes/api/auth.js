8000b75f2425fb7fce6d81a8c3a05445db7edb00
prakharritik <60786008+prakharritik@users.noreply.github.com>
2022-05-10 23:23:53 +0530

    Update auth.js

--- server/routes/api/auth.js
+++ server/routes/api/auth.js
@@ -22,19 +22,34 @@ router.post(
     }),
   ],
   async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
     const { email, password } = req.body;
 
     try {
       let user = await User.findOne({ email });
 
       if (!user) {
-        res.status(400).json({ errors: [{ msg: "Invalid Credentials" }] });
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "Invalid Credentials" }] });
       }
 
       const isMatch = bcrypt.compare(user.password, password);
 
       if (!isMatch) {
-        res.status(400).json({ errors: [{ msg: "Invalid Credentials" }] });
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "Invalid Credentials" }] });
+      }
+
+      if (!user.emailVerified) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "Email Not Verified" }] });
       }
 
       const payload = {
