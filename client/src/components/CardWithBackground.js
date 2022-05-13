62bb9eb85e36868522dab6e5b7dc346d553ebd27
prakharritik <60786008+prakharritik@users.noreply.github.com>
2022-05-13 12:58:07 +0530

    Add Tracks screen.

---
+++ client/src/components/CardWithBackground.js
@@ -0,0 +1,37 @@
+import { ImageBackground, StyleSheet } from "react-native";
+import React from "react";
+import { Text } from "react-native-elements";
+
+const CardWithBackground = ({ uri, heading }) => {
+  return (
+    <ImageBackground
+      source={{
+        uri,
+      }}
+      style={styles.container}
+      imageStyle={{ borderRadius: 10 }}
+    >
+      <Text h3 style={styles.heading}>
+        {heading}
+      </Text>
+    </ImageBackground>
+  );
+};
+
+export default CardWithBackground;
+
+const styles = StyleSheet.create({
+  heading: {
+    color: "#fff",
+    textAlign: "center",
+    backgroundColor: "rgba(0, 0, 255, 0.3)",
+  },
+  container: {
+    height: 100,
+    justifyContent: "center",
+    margin: 10,
+    marginTop: 30,
+    borderRadius: 20,
+    border: 1,
+  },
+});
--- client/src/screens/MytracksScreen.js
+++ client/src/screens/MytracksScreen.js
@@ -1,14 +1,39 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, TouchableOpacity, View } from "react-native";
 import React from "react";
+import { Card, Image, Text } from "react-native-elements";
+import CardWithBackground from "../components/CardWithBackground";
 
-const MytracksScreen = () => {
+const MytracksScreen = ({ navigation }) => {
   return (
     <View>
-      <Text>MytracksScreen</Text>
+      <Text h1 style={styles.heading}>
+        Tracks
+      </Text>
+      <TouchableOpacity onPress={() => navigation.navigate("Progress")}>
+        <CardWithBackground
+          uri="https://images.unsplash.com/photo-1584735935682-2f2b69dff9d2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=871&q=80"
+          heading="Exercise"
+        />
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => navigation.navigate("Progress")}>
+        <CardWithBackground
+          uri="https://images.unsplash.com/photo-1429117237875-aa29229d99f0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+          heading="Sleep"
+        />
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => navigation.navigate("Progress")}>
+        <CardWithBackground
+          uri="https://images.unsplash.com/photo-1561758033-d89a9ad46330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+          heading="Calorie"
+          route="Progress"
+        />
+      </TouchableOpacity>
     </View>
   );
 };
 
 export default MytracksScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  heading: { textAlign: "center" },
+});
